7c1cc68d463752a71a9c16d00aaf242c3be9be08
Pliavi <pliavi.rpgm@gmail.com>
2021-03-26 11:13:19 -0300

    feat: create ProjectBoard component

---
+++ src/components/ui/project/ProjectBoard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project-board">
+    <div class="project-board__title">
+      <h1>{{ project.name }}</h1>
+      <button>+</button>
+    </div>
+
+    <div class="project-board__todos">
+      <TodoItem
+        v-for="todo in project.todos"
+        :project="project"
+        :todo="todo"
+        :key="todo.id"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+  import TodoItem from "@/components/ui/todo/TodoItem";
+
+  export default {
+    props: ["project"],
+    mounted() {},
+    components: { TodoItem }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .project-board__todos {
+    display: flex;
+  }
+</style>
