415244b7a7f180bde2e833f635f8b35f3cab1f90
Pliavi <pliavi.rpgm@gmail.com>
2021-03-26 11:16:32 -0300

    chore: incomplete app before install stylus

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProjectFilters :ordering="options.ordering" :filter="options.filter" />
+    <ProjectBoard :project="currentProject" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { mapGetters, mapMutations } from "vuex";
+  import ProjectFilters from "@/components/ui/project/ProjectFilters";
+  import ProjectBoard from "@/components/ui/project/ProjectBoard";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    components: {
+      ProjectFilters,
+      ProjectBoard
+    },
+    data() {
+      return {
+        options: {
+          ordering: "asc",
+          filter: "all"
+        }
+      };
+    },
+    computed: {
+      ...mapGetters(["currentProject", "filteredTodos"])
+    },
+    methods: {
+      ...mapMutations({ createProject: "createProject" })
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .date {
+  }
 </style>
