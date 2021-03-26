2e40e148b4d4bfee036cb869cee7551d20801cd3
Pliavi <pliavi.rpgm@gmail.com>
2021-03-26 11:13:28 -0300

    feat: create TodoItem component

---
+++ src/components/ui/todo/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="todo-item">
+    <div class="todo-item__icon">
+      {{ todo.task[0] }}
+    </div>
+    <div class="todo-item__info">
+      <div class="todo-item__title">{{ todo.task }}</div>
+      <div class="todo-item__deadline">{{ todo.deadline }}</div>
+    </div>
+    <div class="todo-item__status">
+      {{ todo.completed }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["todo"],
+}
+</script>
