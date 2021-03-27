8fe36bf3640ecdbceb04f0f2aec6bf11f5adc18f
Pliavi <pliavi.rpgm@gmail.com>
2021-03-27 17:52:28 -0300

    feat: finish all filters and sorting logic

--- src/components/ui/project/ProjectFilters.vue
+++ src/components/ui/project/ProjectFilters.vue
@@ -1,29 +1,91 @@
 <template>
-  <div class="project-filters button-bag">
-    <div class="button-bag__upper">
-      <button @click="emitFilter('all')">Todos</button>
-      <button @click="emitFilter('pending')">Pendente</button>
-      <button @click="emitFilter('late')">Atrasado</button>
-      <button @click="emitFilter('completed')">Completado</button>
+  <div
+    class="overflow-hidden bg-white border border-gray-200 divide-y rounded-lg shadow"
+  >
+    <div class="flex divide-x">
+      <button
+        class="flex-1 p-2 hover:bg-gray-100"
+        :class="[filter === 'all' ? 'font-bold bg-gray-100' : '']"
+        @click="emitFilter('all')"
+      >
+        Todos
+      </button>
+      <button
+        class="flex-1 p-2 hover:bg-gray-100"
+        :class="[filter === 'pending' ? 'font-bold bg-gray-100' : '']"
+        @click="emitFilter('pending')"
+      >
+        Pendente
+      </button>
+      <button
+        class="flex-1 p-2 hover:bg-gray-100"
+        :class="[filter === 'late' ? 'font-bold bg-gray-100' : '']"
+        @click="emitFilter('late')"
+      >
+        Atrasado
+      </button>
+      <button
+        class="flex-1 p-2 hover:bg-gray-100"
+        :class="[filter === 'completed' ? 'font-bold bg-gray-100' : '']"
+        @click="emitFilter('completed')"
+      >
+        Completado
+      </button>
     </div>
-    <div class="button-bag__bottom">
-      <button @click="emitOrdering">Data {{ ordering }}</button>
+    <div class="flex divide-x">
+      <button class="flex-1 p-2 hover:bg-gray-100" @click="emitOrder">
+        Data
+        <span class="inline-block w-4" v-if="order === 'none'"> ⚌ </span>
+        <span class="inline-block w-4" v-if="order === 'asc'"> ▲ </span>
+        <span class="inline-block w-4" v-if="order === 'desc'"> ▼ </span>
+      </button>
+      <div class="flex items-center flex-1 gap-4 pl-4 hover:bg-gray-100">
+        <label for="project-select">Projeto:</label>
+        <select
+          id="project-select"
+          class="flex-1 py-2 bg-transparent"
+          @change="emitProject"
+        >
+          <option value="all">Todos</option>
+          <option
+            v-for="(project, index) in projects"
+            :value="index"
+            :key="project.id"
+          >
+            {{ project.name }}
+          </option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ["ordering", "filter"],
-    methods: {
-      emitFilter(state) {
-        this.$emit("filterChange", state);
-      },
-      emitOrdering() {
-        const ordering = this.ordering === "asc" ? "desc" : "asc";
-
-        this.$emit("orderingChange", ordering);
+export default {
+  props: ["order", "filter", "projects"],
+  methods: {
+    emitFilter(filter) {
+      this.$emit("filterChange", filter);
+    },
+    emitOrder() {
+      if (this.order === "none") {
+        this.$emit("orderChange", "asc");
       }
-    }
-  };
+      if (this.order === "asc") {
+        this.$emit("orderChange", "desc");
+      }
+      if (this.order === "desc") {
+        this.$emit("orderChange", "none");
+      }
+    },
+    emitProject(ev) {
+      const projectIndex = ev.target.value;
+      if (projectIndex === "all") {
+        this.$emit("projectChange");
+        return;
+      }
+      this.$emit("projectChange", this.projects[projectIndex]);
+    },
+  },
+};
 </script>
