5669475d1cf380c654f4b09a3810a335babadab3
Pliavi <pliavi.rpgm@gmail.com>
2021-03-27 17:51:44 -0300

    feat: add filter and delete logic to project

--- src/components/ui/project/ProjectBoard.vue
+++ src/components/ui/project/ProjectBoard.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="project-board">
-    <div class="project-board__title">
-      <h1>{{ project.name }}</h1>
-      <button>+</button>
+  <div class="space-y-4">
+    <div class="flex items-center gap-2 px-4">
+      <h1 class="text-2xl font-bold">{{ project.name }}</h1>
+
+      <button
+        class="w-8 h-8 ml-auto text-2xl text-white bg-gray-500 rounded-lg shadow hover:bg-red-800 hover:shadow-md"
+        @click="emitDelete"
+      >
+        ×
+      </button>
     </div>
 
-    <div class="project-board__todos">
+    <NewTodoItem :project="project" />
+
+    <div
+      class="overflow-hidden bg-white divide-y rounded-lg shadow divide-gray-50"
+    >
       <TodoItem
-        v-for="todo in project.todos"
+        v-for="todo in filteredTodos"
         :project="project"
         :todo="todo"
         :key="todo.id"
@@ -17,17 +27,21 @@
 </template>
 
 <script>
-  import TodoItem from "@/components/ui/todo/TodoItem";
+import TodoItem from "@/components/ui/todo/TodoItem";
+import NewTodoItem from "@/components/ui/todo/NewTodo";
 
-  export default {
-    props: ["project"],
-    mounted() {},
-    components: { TodoItem }
-  };
+export default {
+  props: ["project", "order", "filter"],
+  computed: {
+    filteredTodos() {
+      return this.$store.getters.todos(this.project, this.filter, this.order);
+    },
+  },
+  methods: {
+    emitDelete() {
+      this.$emit("delete", this.project);
+    },
+  },
+  components: { TodoItem, NewTodoItem },
+};
 </script>
-
-<style lang="scss" scoped>
-  .project-board__todos {
-    display: flex;
-  }
-</style>
