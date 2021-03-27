6f1ac97a53eb53103517001a38c680d9ee53316e
Pliavi <pliavi.rpgm@gmail.com>
2021-03-27 17:47:13 -0300

    feat: install toast and icons

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import VueToast from "vue-toast-notification";
 
 import "vue2-datepicker/index.css";
 import "vue2-datepicker/locale/pt-br";
+import "vue-toast-notification/dist/theme-sugar.css";
 
 import "@/assets/index.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueToast);
 
 new Vue({
   store,
